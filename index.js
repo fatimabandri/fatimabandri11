2e96f57de94c6095d7d6686bf6ae3c22b1b7d36d
Mehrinwagle <mehrinwagle26@gmail.com>
2025-10-16 16:59:20 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function result(){
+    let a=document.getElementById("num1").value;
+    let b=document.getElementById("num2").value;
+    let c=Number(a) + Number(b);
+
+
+    document.getElementById("result").innerHTML=c;
+
+}
